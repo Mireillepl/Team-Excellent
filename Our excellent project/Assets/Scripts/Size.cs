f2a1b0bea9b3980adcd3b79aa3cf474a6ec8f045
LauriK84 <koholauri@gmail.com>
2021-02-25 11:27:36 +0200

    Wall Destroy

--- Our excellent project/Assets/Scripts/Size.cs
+++ Our excellent project/Assets/Scripts/Size.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Size : MonoBehaviour
 {
+    
+    private void Update()
+    {
+        Debug.Log(transform.localScale.y); 
+       
+    }
+
+
     void OnCollisionEnter(Collision col)  //Unity function called when a collision is detected, and the object collided is put into the variable 'col' to be used later
     {
         if (col.gameObject.tag == "Big")   //if the object you collided with is the enemy
@@ -14,6 +22,11 @@ public class Size : MonoBehaviour
         if (col.gameObject.tag == "Small")   //if the object you collided with is the enemy
         {
             transform.localScale += new Vector3(-1, -1, -1); //increase the size of the ball
+            Destroy(col.gameObject);  //Destroy the enemy
+        }
+        if (col.gameObject.tag == "Wall" && transform.localScale.y == 2)   
+        {
+           
             Destroy(col.gameObject);  //Destroy the enemy
         }
     }
